8af7213d656558f41f89f0c2d39208e803c4cf44
teenrage-dev <renatraiend25@gmail.com>
2023-11-27 14:00:56 +0200

    fix: added basename

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/frontend">
       <ConfigProvider theme={theme}>
         <App />
       </ConfigProvider>
