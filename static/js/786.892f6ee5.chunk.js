0def367926d489d929c32e06d4881b84ebbf3ddd
teenrage-dev <72465313+teenrage-dev@users.noreply.github.com>
2023-12-03 13:03:49 +0000

    Deploying to gh-pages from @ cymdia/frontend@d12271fdbdbdfc51ddb97e1d3aebe53caf13b809 🚀

--- static/js/786.892f6ee5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[786],{9786:(e,n,r)=>{r.r(n),r.d(n,{default:()=>t});var s=r(184);const t=e=>(0,s.jsx)("div",{children:"Creat New"})}}]);
-//# sourceMappingURL=786.892f6ee5.chunk.js.map
